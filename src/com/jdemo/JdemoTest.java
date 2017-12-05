308eb1d711cdd62f2b29423f84bc89cc6b338ab7
binbh3685 <binbh@ithk.com.cn>
2017-12-05 16:28:43 +0800

    谢谢谢

--- src/com/jdemo/JdemoTest.java
+++ src/com/jdemo/JdemoTest.java
@@ -8,7 +8,7 @@ public class JdemoTest {
 		System.out.print("nin haowwww !");
 		
 		
-		System.out.print("nin hKKKaoddddkjk的人!");
+		System.out.print("nin hKKK杀杀杀aoddddkjk的人!");
 
 
 	}
---
+++ src/com/jdemo/master.java
@@ -0,0 +1,10 @@
+package com.jdemo;
+
+public class master {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
