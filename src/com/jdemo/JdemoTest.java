535a1905c798a314ecd73c52205bad8f921f164b
binbh3685 <binbh@ithk.com.cn>
2017-12-07 12:58:05 +0800

    jj

--- src/com/jdemo/JdemoTest.java
+++ src/com/jdemo/JdemoTest.java
@@ -5,7 +5,7 @@ public class JdemoTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("nin haowwwww !");
+		System.out.print("nin ha555owwwww !");
 		System.out.print("nin haowwwww !");
 	
 		System.out.print("nin hKKKddddkjskzzµÄÈË!"+"dddddddddd");
